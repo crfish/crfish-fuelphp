939958c9a9dc091e5f43cde28c820c8a7249a5fb
Kevin McCoy <KevinMcCoy@earthlink.net>
2014-06-07 23:36:41 -0400

    db named to the same as the folder

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=127.0.0.1;dbname=test',
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=crfish-fuelphp',
 			'username'   => 'root',
 			'password'   => 'root',
 		),
